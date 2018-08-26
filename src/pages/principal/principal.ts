1a53fa916666fb75a522cde4d94c3b7839d102c6
Max Alva <alva85@gmail.com>
2018-08-26 15:52:44 -0500

    Navegacion entre paginas

--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { Pagina2Page } from '../pagina2/pagina2';
 
 @Component( {
     selector: 'page-principal',
@@ -7,11 +8,12 @@ import { NavController, NavParams } from 'ionic-angular';
 } )
 export class PrincipalPage {
 
-    constructor( public navCtrl: NavController, public navParams: NavParams ) {
+    pagina2: any = Pagina2Page;
+
+    constructor( public navCtrl: NavController ) {
     }
 
-    ionViewDidLoad() {
-        console.log( 'ionViewDidLoad PrincipalPage' );
+    navegarPagina() {
+        this.navCtrl.push( Pagina2Page );
     }
-
 }
