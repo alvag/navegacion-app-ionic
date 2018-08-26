bb4e2c996a5b2b4080d38ead06595e0638c63731
Max Alva <alva85@gmail.com>
2018-08-26 15:29:35 -0500

    Inicio del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,41 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
-@NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
-})
+import { AjustesPage, Ajustes2Page, ModalPage, Pagina2Page, Pagina3Page, PrincipalPage, TabsPage } from '../pages/index.pages';
+
+@NgModule( {
+    declarations: [
+        MyApp,
+        HomePage,
+        AjustesPage,
+        Ajustes2Page,
+        ModalPage,
+        Pagina2Page,
+        Pagina3Page,
+        PrincipalPage,
+        TabsPage
+
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot( MyApp )
+    ],
+    bootstrap: [ IonicApp ],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        AjustesPage,
+        Ajustes2Page,
+        ModalPage,
+        Pagina2Page,
+        Pagina3Page,
+        PrincipalPage,
+        TabsPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: ErrorHandler, useClass: IonicErrorHandler }
+    ]
+} )
 export class AppModule {}
